c045d62c51993be4084d138533e9b2759dd9828d
sumit-1492 <sumitlala99@gmail.com>
2021-09-14 21:56:00 +0530

    msg

--- src/load_data.py
+++ src/load_data.py
@@ -7,7 +7,7 @@ import argparse
 def load_and_save(config_path):
     config = read_params(config_path)
     df = get_data(config_path)
-    new_cols = [col.replace("_","") for col in df.columns]
+    new_cols = [col.replace(' ','') for col in df.columns]
     raw_data_path = config["load_data"]["raw_dataset_csv"]
     df.to_csv(raw_data_path,sep=",",index=False,header=new_cols)
 
