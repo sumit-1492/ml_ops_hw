1d83a1e0ffe62535691d10c0dd020ecc09e045c5
sumit-1492 <sumitlala99@gmail.com>
2021-09-15 08:42:27 +0530

    msg

---
+++ setup.py
@@ -0,0 +1,11 @@
+from typing_extensions import Literal
+from setuptools import setup, find_packages, version
+
+setup(
+    name = "src",
+    version="0.0.1",
+    description = "simple package",
+    author = "Sumit",
+    packages = find_packages(),
+    license = "MIT"
+)
