c9afae657fe113c48145741de7463a3bad432ae3
sumit-1492 <sumitlala99@gmail.com>
2021-09-16 16:46:45 +0530

    test done

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,16 +1,70 @@
+import json
+import logging
+import os
+import joblib
 import pytest
-class NotInRange(Exception):
-    def __init__(self,message="value not in range"):
-        self.message = message
-        super().__init__(self.message)
+from prediction_service.prediction import form_response, api_response
+import prediction_service
 
-def test_generic():
-    a = 5
-    with pytest.raises(NotInRange):
-        if a not in range(10,20):
-            raise NotInRange
-    
-def test_something():
-    a = 2
-    b = 2
-    assert True
+input_data = {
+    "incorrect_range": 
+    {"Processtemperature[K]": 500, 
+    "Rotationalspeed[rpm]": 3000, 
+    "Torque[Nm]": 100, 
+    "Toolwear[min]": 500, 
+    "TWF": 12, 
+    "HDF": 9, 
+    "PWF": 7, 
+    "OSF": 2, 
+    "RNF": 3, 
+    },
+
+    "correct_range":
+    {"Processtemperature[K]": 310, 
+    "Rotationalspeed[rpm]": 1200, 
+    "Torque[Nm]": 50, 
+    "Toolwear[min]": 100, 
+    "TWF": 1, 
+    "HDF": 0, 
+    "PWF": 1, 
+    "OSF": 0, 
+    "RNF": 0, 
+    },
+
+    "incorrect_col":
+    {"Process temperature[K]": 500, 
+    "Rotational speed[rpm]": 3000, 
+    "Torque [Nm]": 100, 
+    "Tool wear[min]": 500, 
+    "TWF": 12, 
+    "HDF": 9, 
+    "PWF": 7, 
+    "OSF": 2, 
+    "RNF": 3, 
+    }
+}
+
+TARGET_range = {
+    "min": 295.3,
+    "max": 304.5
+}
+
+def test_form_response_correct_range(data=input_data["correct_range"]):
+    res = form_response(data)
+    assert  TARGET_range["min"] <= res <= TARGET_range["max"]
+
+def test_api_response_correct_range(data=input_data["correct_range"]):
+    res = api_response(data)
+    assert  TARGET_range["min"] <= res["response"] <= TARGET_range["max"]
+
+def test_form_response_incorrect_range(data=input_data["incorrect_range"]):
+    with pytest.raises(prediction_service.prediction.NotInRange):
+        res = form_response(data)
+
+def test_api_response_incorrect_range(data=input_data["incorrect_range"]):
+    res = api_response(data)
+    assert res["response"] == prediction_service.prediction.NotInRange().message
+
+def test_api_response_incorrect_col(data=input_data["incorrect_col"]):
+    res = api_response(data)
+    assert res["response"] == prediction_service.prediction.NotInCols().message
